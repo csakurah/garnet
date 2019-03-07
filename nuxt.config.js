0f91a22db4971c7d516e00b710d3de123a98305f
csakurah <aqua877@gmail.com>
2019-03-07 11:57:50 +0900

    moved apiKey config

---
+++ nuxt.config.js
@@ -0,0 +1,51 @@
+const pkg = require('./package')
+const config = require('./config/config.dev')
+
+module.exports = {
+  mode: 'spa',
+  buildDir: './public',
+  head: {
+    title: 'Garnet',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  loading: { color: '#fff' },
+  css: [{ src: '~assets/main.scss', lang: 'scss' }],
+  plugins: [],
+  modules: ['@nuxtjs/bulma', 'nuxt-fontawesome'],
+  build: {
+    postcss: {
+      preset: {
+        features: {
+          customProperties: false
+        }
+      }
+    },
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  fontawesome: {
+    imports: [
+      {
+        set: '@fortawesome/free-solid-svg-icons',
+        icons: ['fas']
+      }
+    ]
+  },
+  env: {
+    firebase: config.firebase
+  }
+}
